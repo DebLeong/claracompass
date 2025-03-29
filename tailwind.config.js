4ce308bdf388e3ad56560373b7d4327692401257
DebLeong <37959355+DebLeong@users.noreply.github.com>
2025-03-29 19:55:11 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blush: "#DAB6B6",
+        ivory: "#FAF6F0",
+        clay: "#B76E79"
+      },
+      fontFamily: {
+        serif: ['"Playfair Display"', "serif"]
+      }
+    }
+  },
+  plugins: []
+}
