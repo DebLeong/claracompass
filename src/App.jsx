e3ab9fa8094815e6c74e25c79d0ffc84f9d0fb30
DebLeong <37959355+DebLeong@users.noreply.github.com>
2025-03-29 18:27:59 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "./components/HeroSection";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <main>
+      <HeroSection />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
