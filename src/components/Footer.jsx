d3337bb4a5c1393c06a86010adadddeb3e298c78
DebLeong <37959355+DebLeong@users.noreply.github.com>
2025-03-29 19:54:18 +0800

    Delete src/components/Footer.jsx

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center py-8 bg-white text-gray-500 text-sm">
-      © {new Date().getFullYear()} Clara Compass. All rights reserved.
-    </footer>
-  );
-}
